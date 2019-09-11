b8d7f821212a0b44aff95005f4a8ae7cfc9d4ed2
Phong Pham <phongpham1342@hotmail.com>
2019-09-10 23:45:04 -0400

    Initial commit

---
+++ A1/Main.cpp
@@ -0,0 +1,9 @@
+// Fall 2019
+
+#include "A1.hpp"
+
+int main( int argc, char **argv ) 
+{
+	CS488Window::launch( argc, argv, new A1(), 1024, 768, "F19 Assignment 1" );
+	return 0;
+}
---
+++ A4/scene_lua.hpp
@@ -0,0 +1,7 @@
+// Fall 2019
+
+#pragma once
+
+#include <string>
+
+bool run_lua( const std::string& filename );
